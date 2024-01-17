d2d3ef488b8e1e903714327c06ed2c0d06edf959
AlexaGabriel <alexandre181828@gmail.com>
2024-01-17 09:13:27 -0300

    melhor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
     colors: {
       bluep: "hsl(252, 100%, 67%)",
       bluepr: "hsl(241, 81%, 54%)",
